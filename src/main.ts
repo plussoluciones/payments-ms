0a6cdbc6dc110b5372e20c3454278fd6aaca8706
Edgardo Alvarez <edgardo@livedesign.org>
2024-05-13 08:21:32 -0500

    routas creadas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Paymets-ms');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+
+  logger.log(`Payments Microservice running on port ${envs.port}`);
 }
 bootstrap();
